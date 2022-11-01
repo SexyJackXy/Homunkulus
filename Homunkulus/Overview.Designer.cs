ed495de0bc5c670dd2b9a18d26e5cdab85ea1b24
SexyJackXy <tjachmann26@gmail.com>
2022-11-01 02:27:31 +0100

    Added new Desinge

--- Homunkulus/Overview.Designer.cs
+++ Homunkulus/Overview.Designer.cs
@@ -35,23 +35,23 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(58)))), ((int)(((byte)(73)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(174)))), ((int)(((byte)(174)))));
             this.panel1.Location = new System.Drawing.Point(-3, -1);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(96, 541);
+            this.panel1.Size = new System.Drawing.Size(76, 541);
             this.panel1.TabIndex = 0;
             // 
             // create_backup_btn
             // 
-            this.create_backup_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(202)))), ((int)(((byte)(217)))));
+            this.create_backup_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(103)))), ((int)(((byte)(198)))));
             this.create_backup_btn.FlatAppearance.BorderSize = 0;
             this.create_backup_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(170)))), ((int)(((byte)(185)))));
             this.create_backup_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.create_backup_btn.ForeColor = System.Drawing.Color.White;
-            this.create_backup_btn.Location = new System.Drawing.Point(417, 493);
+            this.create_backup_btn.Location = new System.Drawing.Point(380, 490);
             this.create_backup_btn.Name = "create_backup_btn";
-            this.create_backup_btn.Size = new System.Drawing.Size(134, 35);
+            this.create_backup_btn.Size = new System.Drawing.Size(167, 38);
             this.create_backup_btn.TabIndex = 1;
             this.create_backup_btn.Text = "Create Backup";
             this.create_backup_btn.UseVisualStyleBackColor = false;
@@ -59,14 +59,14 @@
             // 
             // import_backup_btn
             // 
-            this.import_backup_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(160)))), ((int)(((byte)(228)))));
+            this.import_backup_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(103)))), ((int)(((byte)(198)))));
             this.import_backup_btn.FlatAppearance.BorderSize = 0;
             this.import_backup_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(105)))), ((int)(((byte)(198)))));
             this.import_backup_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.import_backup_btn.ForeColor = System.Drawing.Color.White;
-            this.import_backup_btn.Location = new System.Drawing.Point(574, 493);
+            this.import_backup_btn.Location = new System.Drawing.Point(578, 490);
             this.import_backup_btn.Name = "import_backup_btn";
-            this.import_backup_btn.Size = new System.Drawing.Size(134, 35);
+            this.import_backup_btn.Size = new System.Drawing.Size(167, 38);
             this.import_backup_btn.TabIndex = 2;
             this.import_backup_btn.Text = "Import Backup";
             this.import_backup_btn.UseVisualStyleBackColor = false;
@@ -76,7 +76,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(154)))), ((int)(((byte)(169)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(1029, 540);
             this.Controls.Add(this.import_backup_btn);
             this.Controls.Add(this.create_backup_btn);
