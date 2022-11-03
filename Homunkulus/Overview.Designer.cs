3620fe5363d547ba8e02c32a22b996495e84aa65
SexyJackXy <tjachmann26@gmail.com>
2022-11-03 16:50:43 +0100

    Added Funtion
    Function:
    Now you can run Backups

--- Homunkulus/Overview.Designer.cs
+++ Homunkulus/Overview.Designer.cs
@@ -31,13 +31,16 @@
             this.create_backup_btn = new System.Windows.Forms.Button();
             this.import_backup_btn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.source_txt = new System.Windows.Forms.TextBox();
-            this.src_btn = new System.Windows.Forms.Button();
+            this.Destination_txt = new System.Windows.Forms.TextBox();
+            this.add_folder_btn = new System.Windows.Forms.Button();
             this.Source_btn = new System.Windows.Forms.Label();
             this.Destination_btn = new System.Windows.Forms.Label();
-            this.dest_btn = new System.Windows.Forms.Button();
+            this.dest_folder_btn = new System.Windows.Forms.Button();
             this.source_rtb = new System.Windows.Forms.RichTextBox();
             this.start_btn = new System.Windows.Forms.Button();
+            this.multiple_folder_btn = new System.Windows.Forms.Button();
+            this.add_data_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // create_backup_btn
@@ -48,13 +51,12 @@
             this.create_backup_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
             this.create_backup_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.create_backup_btn.ForeColor = System.Drawing.Color.White;
-            this.create_backup_btn.Location = new System.Drawing.Point(12, 402);
+            this.create_backup_btn.Location = new System.Drawing.Point(251, 559);
             this.create_backup_btn.Name = "create_backup_btn";
-            this.create_backup_btn.Size = new System.Drawing.Size(271, 40);
+            this.create_backup_btn.Size = new System.Drawing.Size(234, 40);
             this.create_backup_btn.TabIndex = 1;
             this.create_backup_btn.Text = "Create Backup";
             this.create_backup_btn.UseVisualStyleBackColor = false;
-            this.create_backup_btn.Click += new System.EventHandler(this.create_backup_btn_Click);
             // 
             // import_backup_btn
             // 
@@ -64,54 +66,52 @@
             this.import_backup_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
             this.import_backup_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.import_backup_btn.ForeColor = System.Drawing.Color.White;
-            this.import_backup_btn.Location = new System.Drawing.Point(288, 402);
+            this.import_backup_btn.Location = new System.Drawing.Point(491, 559);
             this.import_backup_btn.Name = "import_backup_btn";
-            this.import_backup_btn.Size = new System.Drawing.Size(268, 40);
+            this.import_backup_btn.Size = new System.Drawing.Size(234, 40);
             this.import_backup_btn.TabIndex = 2;
             this.import_backup_btn.Text = "Import Backup";
             this.import_backup_btn.UseVisualStyleBackColor = false;
-            this.import_backup_btn.Click += new System.EventHandler(this.import_backup_btn_Click);
             // 
             // panel1
             // 
             this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(233)))), ((int)(((byte)(245)))));
-            this.panel1.Location = new System.Drawing.Point(-3, 495);
+            this.panel1.Location = new System.Drawing.Point(-3, 606);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(571, 45);
+            this.panel1.Size = new System.Drawing.Size(982, 40);
             this.panel1.TabIndex = 0;
             // 
-            // source_txt
+            // Destination_txt
             // 
-            this.source_txt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(233)))), ((int)(((byte)(245)))));
-            this.source_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.source_txt.Location = new System.Drawing.Point(12, 136);
-            this.source_txt.Name = "source_txt";
-            this.source_txt.Size = new System.Drawing.Size(510, 26);
-            this.source_txt.TabIndex = 3;
+            this.Destination_txt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(233)))), ((int)(((byte)(245)))));
+            this.Destination_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Destination_txt.Location = new System.Drawing.Point(12, 30);
+            this.Destination_txt.Name = "Destination_txt";
+            this.Destination_txt.Size = new System.Drawing.Size(794, 26);
+            this.Destination_txt.TabIndex = 3;
             // 
-            // src_btn
+            // add_folder_btn
             // 
-            this.src_btn.AutoSize = true;
-            this.src_btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.src_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
-            this.src_btn.FlatAppearance.BorderSize = 0;
-            this.src_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
-            this.src_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.src_btn.ForeColor = System.Drawing.Color.White;
-            this.src_btn.Location = new System.Drawing.Point(526, 73);
-            this.src_btn.Name = "src_btn";
-            this.src_btn.Size = new System.Drawing.Size(30, 28);
-            this.src_btn.TabIndex = 4;
-            this.src_btn.Text = "...";
-            this.src_btn.UseVisualStyleBackColor = false;
-            this.src_btn.Click += new System.EventHandler(this.src_btn_Click);
+            this.add_folder_btn.AutoSize = true;
+            this.add_folder_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
+            this.add_folder_btn.FlatAppearance.BorderSize = 0;
+            this.add_folder_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.add_folder_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_folder_btn.ForeColor = System.Drawing.Color.White;
+            this.add_folder_btn.Location = new System.Drawing.Point(812, 146);
+            this.add_folder_btn.Name = "add_folder_btn";
+            this.add_folder_btn.Size = new System.Drawing.Size(153, 28);
+            this.add_folder_btn.TabIndex = 4;
+            this.add_folder_btn.Text = "Add Folder";
+            this.add_folder_btn.UseVisualStyleBackColor = false;
+            this.add_folder_btn.Click += new System.EventHandler(this.src_btn_Click);
             // 
             // Source_btn
             // 
             this.Source_btn.AutoSize = true;
-            this.Source_btn.Location = new System.Drawing.Point(12, 7);
+            this.Source_btn.Location = new System.Drawing.Point(12, 91);
             this.Source_btn.Name = "Source_btn";
             this.Source_btn.Size = new System.Drawing.Size(86, 18);
             this.Source_btn.TabIndex = 5;
@@ -120,37 +120,37 @@
             // Destination_btn
             // 
             this.Destination_btn.AutoSize = true;
-            this.Destination_btn.Location = new System.Drawing.Point(12, 115);
+            this.Destination_btn.Location = new System.Drawing.Point(12, 9);
             this.Destination_btn.Name = "Destination_btn";
             this.Destination_btn.Size = new System.Drawing.Size(184, 18);
             this.Destination_btn.TabIndex = 8;
             this.Destination_btn.Text = "Select Destination Folder";
             // 
-            // dest_btn
+            // dest_folder_btn
             // 
-            this.dest_btn.AutoSize = true;
-            this.dest_btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.dest_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
-            this.dest_btn.FlatAppearance.BorderSize = 0;
-            this.dest_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
-            this.dest_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.dest_btn.ForeColor = System.Drawing.Color.White;
-            this.dest_btn.Location = new System.Drawing.Point(528, 133);
-            this.dest_btn.Name = "dest_btn";
-            this.dest_btn.Size = new System.Drawing.Size(30, 28);
-            this.dest_btn.TabIndex = 7;
-            this.dest_btn.Text = "...";
-            this.dest_btn.UseVisualStyleBackColor = false;
+            this.dest_folder_btn.AutoSize = true;
+            this.dest_folder_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
+            this.dest_folder_btn.FlatAppearance.BorderSize = 0;
+            this.dest_folder_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.dest_folder_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dest_folder_btn.ForeColor = System.Drawing.Color.White;
+            this.dest_folder_btn.Location = new System.Drawing.Point(812, 30);
+            this.dest_folder_btn.Name = "dest_folder_btn";
+            this.dest_folder_btn.Size = new System.Drawing.Size(153, 28);
+            this.dest_folder_btn.TabIndex = 7;
+            this.dest_folder_btn.Text = "Add Folder";
+            this.dest_folder_btn.UseVisualStyleBackColor = false;
+            this.dest_folder_btn.Click += new System.EventHandler(this.dest_folder_btn_Click);
             // 
             // source_rtb
             // 
             this.source_rtb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(233)))), ((int)(((byte)(245)))));
             this.source_rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.source_rtb.EnableAutoDragDrop = true;
-            this.source_rtb.Location = new System.Drawing.Point(12, 28);
+            this.source_rtb.Location = new System.Drawing.Point(12, 112);
             this.source_rtb.Name = "source_rtb";
             this.source_rtb.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.source_rtb.Size = new System.Drawing.Size(510, 73);
+            this.source_rtb.Size = new System.Drawing.Size(794, 393);
             this.source_rtb.TabIndex = 9;
             this.source_rtb.TabStop = false;
             this.source_rtb.Text = "";
@@ -163,26 +163,76 @@
             this.start_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
             this.start_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.start_btn.ForeColor = System.Drawing.Color.White;
-            this.start_btn.Location = new System.Drawing.Point(12, 448);
+            this.start_btn.Location = new System.Drawing.Point(12, 559);
             this.start_btn.Name = "start_btn";
-            this.start_btn.Size = new System.Drawing.Size(547, 40);
+            this.start_btn.Size = new System.Drawing.Size(234, 40);
             this.start_btn.TabIndex = 10;
             this.start_btn.Text = "Start Backup";
             this.start_btn.UseVisualStyleBackColor = false;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // multiple_folder_btn
+            // 
+            this.multiple_folder_btn.AutoSize = true;
+            this.multiple_folder_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
+            this.multiple_folder_btn.FlatAppearance.BorderSize = 0;
+            this.multiple_folder_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.multiple_folder_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.multiple_folder_btn.ForeColor = System.Drawing.Color.White;
+            this.multiple_folder_btn.Location = new System.Drawing.Point(812, 112);
+            this.multiple_folder_btn.Name = "multiple_folder_btn";
+            this.multiple_folder_btn.Size = new System.Drawing.Size(153, 28);
+            this.multiple_folder_btn.TabIndex = 11;
+            this.multiple_folder_btn.Text = "Add Multiple Folder";
+            this.multiple_folder_btn.UseVisualStyleBackColor = false;
+            // 
+            // add_data_btn
+            // 
+            this.add_data_btn.AutoSize = true;
+            this.add_data_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
+            this.add_data_btn.FlatAppearance.BorderSize = 0;
+            this.add_data_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.add_data_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_data_btn.ForeColor = System.Drawing.Color.White;
+            this.add_data_btn.Location = new System.Drawing.Point(812, 180);
+            this.add_data_btn.Name = "add_data_btn";
+            this.add_data_btn.Size = new System.Drawing.Size(153, 28);
+            this.add_data_btn.TabIndex = 12;
+            this.add_data_btn.Text = "Add Data";
+            this.add_data_btn.UseVisualStyleBackColor = false;
+            this.add_data_btn.Click += new System.EventHandler(this.add_data_btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(19)))), ((int)(((byte)(54)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(10)))), ((int)(((byte)(50)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(731, 559);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 40);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Save Backup";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Overview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(568, 540);
+            this.ClientSize = new System.Drawing.Size(977, 644);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.add_data_btn);
+            this.Controls.Add(this.multiple_folder_btn);
             this.Controls.Add(this.start_btn);
             this.Controls.Add(this.source_rtb);
             this.Controls.Add(this.Destination_btn);
-            this.Controls.Add(this.dest_btn);
+            this.Controls.Add(this.dest_folder_btn);
             this.Controls.Add(this.Source_btn);
-            this.Controls.Add(this.src_btn);
-            this.Controls.Add(this.source_txt);
+            this.Controls.Add(this.add_folder_btn);
+            this.Controls.Add(this.Destination_txt);
             this.Controls.Add(this.import_backup_btn);
             this.Controls.Add(this.create_backup_btn);
             this.Controls.Add(this.panel1);
@@ -198,12 +248,15 @@
         private Button create_backup_btn;
         private Button import_backup_btn;
         private Panel panel1;
-        private TextBox source_txt;
-        private Button src_btn;
+        private TextBox Destination_txt;
+        private Button add_folder_btn;
         private Label Source_btn;
         private Label Destination_btn;
-        private Button dest_btn;
+        private Button dest_folder_btn;
         private RichTextBox source_rtb;
         private Button start_btn;
+        private Button multiple_folder_btn;
+        private Button add_data_btn;
+        private Button button1;
     }
 }
--- Homunkulus/Overview.cs
+++ Homunkulus/Overview.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -21,17 +22,39 @@ namespace Homunkulus
         {
             InitializeComponent();
         }
-
-        private void create_backup_btn_Click(object sender, EventArgs e)
+        public static string elapsedTime { get; set; }
+        public static void Copy(string sourceDirectory, string targetDirectory)
         {
+            var diSource = new DirectoryInfo(sourceDirectory);
+            var diTarget = new DirectoryInfo(targetDirectory);
 
+            CopyAll(diSource, diTarget);
         }
-
-        private void import_backup_btn_Click(object sender, EventArgs e)
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
         {
+            Directory.CreateDirectory(target.FullName);
 
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+            try
+            {
+                // Copy each subdirectory using recursion.
+                foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+                {
+                    DirectoryInfo nextTargetSubDir =
+                        target.CreateSubdirectory(diSourceSubDir.Name);
+                    CopyAll(diSourceSubDir, nextTargetSubDir);
+                }
+            }
+            catch
+            {
+
+            }
         }
-
         private void src_btn_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
@@ -51,12 +74,123 @@ namespace Homunkulus
                     {
                         string short_path = folder.Remove(0, 1);
                         source_rtb.Text += short_path + "\n";
+                        source_rtb.SelectAll();
+                        source_rtb.SelectionAlignment = HorizontalAlignment.Left;
+                        source_rtb.DeselectAll();
                         folderlist.Add(short_path);
                     }
                     else
                     {
                         source_rtb.Text += folder + "\n";
+                        source_rtb.SelectAll();
+                        source_rtb.SelectionAlignment = HorizontalAlignment.Left;
+                        source_rtb.DeselectAll();
                         folderlist.Add(folder);
+                    }
+                }
+            }
+        }
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();  
+            stopwatch.Start();
+
+            source_rtb = null;
+
+            DateTime datetime = DateTime.Today;
+
+            string destpath = Destination_txt.Text;
+            string date = datetime.ToString("dd/MM/yyyy");
+            string dest = destpath + date;
+            string shrt;
+
+            Directory.CreateDirectory(dest);
+
+            if (folderlist.Count > 0)
+            {
+                for(int i = 0; i < folderlist.Count; i++)
+                {
+                    string sourceDirectory = folderlist.ElementAt(i);
+                    shrt = sourceDirectory.Substring(sourceDirectory.LastIndexOf("\\") + 1);
+                    string subfolder = destpath + "/Backup " + date + "/" + shrt;
+                    string targetDirectory = subfolder;
+
+                    MessageBox.Show(sourceDirectory, targetDirectory);
+
+                    if (Directory.Exists(sourceDirectory))
+                    {
+                        Directory.CreateDirectory(subfolder);
+                        Copy(sourceDirectory, targetDirectory);
+                    }
+                }
+            }
+
+            stopwatch.Stop();                       //Stop der Stopuhr
+            TimeSpan ts = stopwatch.Elapsed;        //Zeitspanne der Stopuhr in eine Variable umwandeln
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds / 10);
+
+            source_rtb.Text += Environment.NewLine + "Backup Finished";
+        }
+        private void add_data_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string Fullpath = ofd.FileName;
+                string Filename = ofd.SafeFileName;
+                string folder = Fullpath.Replace(Filename, "");
+                var regex = new Regex(@"\s");
+
+                if (String.IsNullOrEmpty(folder))
+                {
+                    MessageBox.Show("You can not add nothing");
+                }
+                else
+                {
+                    if (regex.IsMatch(folder))
+                    {
+                        string short_path = folder.Remove(0, 1);
+                        source_rtb.Text += short_path + "\n";
+                        source_rtb.SelectAll();
+                        source_rtb.SelectionAlignment = HorizontalAlignment.Left;
+                        source_rtb.DeselectAll();
+                        folderlist.Add(short_path);
+                    }
+                    else
+                    {
+                        source_rtb.Text += folder + "\n";
+                        source_rtb.SelectAll();
+                        source_rtb.SelectionAlignment = HorizontalAlignment.Left;
+                        source_rtb.DeselectAll();
+                        folderlist.Add(folder);
+                    }
+
+                }
+            }
+        }
+        private void dest_folder_btn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string folder = fbd.SelectedPath;
+                var regex = new Regex(@"\s");
+
+                if (String.IsNullOrEmpty(folder))
+                {
+                    MessageBox.Show("You can not add nothing");
+                }
+                else
+                {
+                    if (regex.IsMatch(folder))
+                    {
+                        Destination_txt.Text = folder;
+                    }
+                    else
+                    {
+                        Destination_txt.Text = folder;
                     }
                 }
             }
