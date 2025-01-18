01b0e964b45a0c3050b1e71df9b8626251ab5c6e
Tim <tjachmann26@gmail.com>
2025-01-18 20:36:04 +0100

    Refactoring: Removed unessarry Usings

--- Homunkulus/Helper/StrExtension.cs
+++ Homunkulus/Helper/StrExtension.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homunkulus.Helper
 {
--- Homunkulus/Helper/Util.cs
+++ Homunkulus/Helper/Util.cs
@@ -1,11 +1,4 @@
 ﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homunkulus.Helper
 {
