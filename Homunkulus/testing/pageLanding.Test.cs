8fab4cf20e4130fbc9462487ed02ff199ef73087
Tim <tjachmann26@gmail.com>
2025-02-21 22:04:06 +0100

    pageLandingTest: Create Initial File

---
+++ Homunkulus/testing/pageLanding.Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homunkulus.testing
+{
+    internal class pageLanding
+    {
+        //TODO: Mach dich schlau wie du am dümmsten dein Code Testen kannst
+    }
+}
